2378d6f6781cff72733a7f29be02cf6a94a2431e
JankDev <robert.s.kraut@gmail.com>
2019-02-07 22:15:11 +0100

    demo mail service

---
+++ powiesciREST/src/main/java/pl/powiescdosukcesu/mail/EmailService.java
@@ -0,0 +1,12 @@
+package pl.powiescdosukcesu.mail;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+}
